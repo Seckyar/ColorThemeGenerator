b9b0629d607703b2dfe42d678c5125c0a6b0bbac
Seckyar <xero.mz93@gmail.com>
2025-07-22 23:55:26 +0630

    V 1.10

--- script.js
+++ script.js
@@ -199,20 +199,6 @@ async function loadGoogleFonts(selectedFont = '') {
 
 saturationValue.textContent = `${saturationSlider.value}%`;
 
-const openBtn = document.getElementById('openAboutBtn');
-const modal = document.getElementById('aboutModal');
-const closeBtn = document.getElementById('closeAboutBtn');
 
-openBtn.addEventListener('click', () => {
-  modal.style.display = 'block';
-});
-
-closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-window.addEventListener('click', (e) => {
-  if (e.target === modal) modal.style.display = 'none';
-});
 
 
